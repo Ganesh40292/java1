88a58a9283740cec5752bb085ce2fe326501d62a
GANESH PRASAD <ganesh.23cs040@sode-edu.in>
2024-12-10 15:20:23 +0530

    Add files via upload

---
+++ MyPackage.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2;
+
+
+public class MyPackage {
+    public void display()
+        {
+		System.out.println("This msg is from MyPackage class within pkg2");
+	}
+    
+}
---
+++ MyPackage3.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3;
+
+/**
+ *
+ * @author Acer
+ */
+public class MyPackage3 {
+    public void display()
+        {
+		System.out.println("This msg is from MyPackage3 class within pkg3");
+	}
+    
+}
